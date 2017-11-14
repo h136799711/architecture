1ee05dbf1a5e9e52a1d4757c13f88304f8854ccc
hebidu <hebiduhebi@126.com>
2017-11-14 10:04:16 +0800

    【更新】优化了消息队列的调用

---
+++ src/component/bookstore/v1/entity/BookEntity.php
@@ -0,0 +1,139 @@
+<?php
+/**
+ * 注意：本内容仅限于博也公司内部传阅,禁止外泄以及用于其他的商业目的
+ * @author    hebidu<346551990@qq.com>
+ * @copyright 2017 www.itboye.com Boye Inc. All rights reserved.
+ * @link      http://www.itboye.com/
+ * @license   http://www.opensource.org/licenses/mit-license.php MIT License
+ * Revision History Version
+ ********1.0.0********************
+ * file created @ 2017-11-09 10:51
+ *********************************
+ ********1.0.1********************
+ *
+ *********************************
+ */
+
+namespace by\component\bookstore\v1;
+
+
+use by\infrastructure\base\BaseEntity;
+
+/**
+ * Class BookEntity
+ * 书本实体
+ * @package by\component\bookstore\v1
+ */
+class BookEntity extends BaseEntity
+{
+    private $title;
+    private $authorId;
+    private $authorName;
+    private $desc;
+    private $thumbnail;
+    private $serialized;
+
+    public function __construct()
+    {
+        // TODO construct
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorId()
+    {
+        return $this->authorId;
+    }
+
+    /**
+     * @param mixed $authorId
+     */
+    public function setAuthorId($authorId)
+    {
+        $this->authorId = $authorId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAuthorName()
+    {
+        return $this->authorName;
+    }
+
+    /**
+     * @param mixed $authorName
+     */
+    public function setAuthorName($authorName)
+    {
+        $this->authorName = $authorName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDesc()
+    {
+        return $this->desc;
+    }
+
+    /**
+     * @param mixed $desc
+     */
+    public function setDesc($desc)
+    {
+        $this->desc = $desc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getThumbnail()
+    {
+        return $this->thumbnail;
+    }
+
+    /**
+     * @param mixed $thumbnail
+     */
+    public function setThumbnail($thumbnail)
+    {
+        $this->thumbnail = $thumbnail;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSerialized()
+    {
+        return $this->serialized;
+    }
+
+    // construct
+
+    /**
+     * @param mixed $serialized
+     */
+    public function setSerialized($serialized)
+    {
+        $this->serialized = $serialized;
+    }
+
+}
